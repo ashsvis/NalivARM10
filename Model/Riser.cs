36d132d87356dba673d6bef5026a8d8cb7723205
ashsvis <ashsvis@gmail.com>
2020-12-27 13:27:27 +0300

    Класс

---
+++ Model/Riser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NalivARM10.Model
+{
+    public class Riser
+    {
+        public Guid OverpassId { get; set; }
+        public Guid ProductId { get; set; }
+        public uint Number { get; set; }
+        public byte NodeAddr { get; set; }
+    }
+}
